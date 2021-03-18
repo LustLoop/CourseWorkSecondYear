288fe9885a88d57196e8b923996a6fc38f3a21fc
LustLoop <lustloop@gmail.com>
2021-03-18 18:34:15 +0200

    Implement work with repository inside of controller (kind of)

--- src/main/java/com/example/demo/product/ProductController.java
+++ src/main/java/com/example/demo/product/ProductController.java
@@ -1,18 +1,48 @@
 package com.example.demo.product;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+    private ProductRepository productRepository;
+
+    @Autowired
+    private void setProductRepository(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     @GetMapping
-    public Product getProducts() {
-        return new Tool(null,"Name", new BigDecimal("800.08"), "Battery", "80%", ToolType.OTHER,true, true);
+    public List<Product> getProducts() {
+        Product product1 = new Tool(null,
+                "Some kind of screwdriver",
+                new BigDecimal("800.08"),
+                "Battery",
+                "80%",
+                ToolType.OTHER,
+                false,
+                false);
+
+        Product product2 = new Tool(null,
+                "Dunno",
+                new BigDecimal("55550.08"),
+                "Make energy sources enums, maybe??",
+                "100%",
+                ToolType.HAND_TOOL,
+                false,
+                true);
+
+        productRepository.addNewProduct(product1);
+        productRepository.addNewProduct(product2);
+
+        return productRepository.getAllProducts();
     }
 }
